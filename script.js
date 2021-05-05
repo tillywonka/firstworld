722422a4a586ef64590c5076a05358cbb46aeb85
tillywonka <83503301+tillywonka@users.noreply.github.com>
2021-05-05 12:42:50 +0000

    Deploying to gh-pages from master @ 17901caeec84f6e2b17bf5f85c271f402443aae5 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully")})();
+(()=>{"use strict";console.log("Die ersten drei, die das lesen und sich bei Steve oder Tilly melden, bekommen ein HighFive+ sobald diese verfügbar sind.")})();
 //# sourceMappingURL=script.js.map
