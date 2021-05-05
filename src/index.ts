bffa71d4bee6a412e769828a4169ef314bf0cdbc
tillywonka <83503301+tillywonka@users.noreply.github.com>
2021-05-05 15:39:12 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
 
-console.log('Du hast die Konsole entdeckt :-O Bist du eine Hacker?? Jetzt aber schnell. Sage Steve oder Tilly, welche Musik im Hühnerraum läuft. Die ersten drei erhalten ein HighFive+ aus unserem persönlichen HighFive+ Kontingent');
+console.log('Du hast die Konsole entdeckt :-O Bist du eine Hacker?? Jetzt aber schnell. Finde heraus unter wievielen ?-Blöcken Nyan-Mario im Hühnerraum durchläuft? Die ersten drei richtigen Meldungen bei Steve oder Tilly erhalten ein HighFive+ aus unserem persönlichen HighFive+ Kontingent');
